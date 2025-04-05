6fc675d88daff67b824a9583779026bf0c33361c
Nurikcmd <mr.nursat04@gmail.com>
2025-04-06 03:54:46 +0500

    Инициализация проекта: базовая структура MainActivity и layou

--- app/src/main/java/com/example/lab9/MainActivity.java
+++ app/src/main/java/com/example/lab9/MainActivity.java
@@ -7,8 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText randomCharacterEditText;
+    private BroadcastReceiver broadcastReceiver;
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        
+
+    }
+
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
     }
 }
